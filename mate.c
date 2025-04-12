c95bc2e40ea02cc0cab9670f39688e84e7f3c760
TomasBorquez <tomasborquez13@gmail.com>
2025-04-12 17:57:19 -0300

    Update deps

--- mate.c
+++ mate.c
@@ -6,13 +6,13 @@ i32 main() {
   {
     CreateExecutable((Executable){.output = S("main"), .flags = S("-Wall -ggdb")});
 
-    AddFile(S("./src/main.c | ./src/renderer.h"));
+    AddFile(S("./src/main.c"));
     AddFile(S("./src/file.c"));
     AddFile(S("./src/base.c"));
     AddFile(S("./src/debugger.c"));
     AddFile(S("./src/disassembly.c"));
 
-    AddIncludePaths(S("C:/raylib/include"), S("./src"), S("./"), S("./vendor"), S("./vendor/capstone"));
+    AddIncludePaths(S("C:/raylib/include"), S("./src"), S("./"), S("./vendor"), S("./vendor/capstone"), S("./vendor/renderer"));
     AddLibraryPaths(S("C:/raylib/lib"), S("./vendor/capstone"));
     LinkSystemLibraries(S("capstone"), S("raylib"), S("opengl32"), S("gdi32"), S("winmm"));
 
